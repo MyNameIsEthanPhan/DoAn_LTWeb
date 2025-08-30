d1b9fc529197da8a1e028093680434557eda5553
EthanPhan <ethanphan.dev@gmail.com>
2025-08-30 20:48:59 +0700

    layout update

--- Website_E_Commerce/App_Start/BundleConfig.cs
+++ Website_E_Commerce/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Website_E_Commerce
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/layout.css"));
         }
     }
 }
